5cde2fd78c7c0633245d3bdf487811e9dcaf60c1
ianmadiana <ian.madiana@students.utdi.ac.id>
2023-03-29 10:52:44 +0700

    app - BottomNavBar

---
+++ bottom_nav_bar_30/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late int index;
+
+  List showWidgets = [
+    Center(
+      child: Text("Home"),
+    ),
+    Center(
+      child: Text("Profile"),
+    ),
+    Center(
+      child: Text("Setting"),
+    ),
+  ];
+
+  @override
+  void initState() {
+    index = 0;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Bottom Navigation Bar"),
+      ),
+      body: showWidgets[index],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Setting'),
+        ],
+        currentIndex: index,
+        onTap: (value) {
+          setState(() {
+            index = value;
+          });
+        },
+      ),
+    );
+  }
+}
